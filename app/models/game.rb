5849897cc7485530b7f64a6e4261cc4bf8035f22
loren <lorenmichaelh@gmail.com>
2022-09-29 21:27:27 -0500

    add img column to games table, seeding issues

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,7 +2,8 @@ class Game < ActiveRecord::Base
   has_many :characters
   has_many :users, through: :characters
 
-  def initialize(name:)
-    @name = name
+  def initialize(game_name:, img:)
+    @game_name = game_name
+    @img = img
   end
 end
