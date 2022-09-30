2d5e28042658a888d385887da95e9e1ef7ea5a25
loren <lorenmichaelh@gmail.com>
2022-09-29 21:43:15 -0500

    removed init from classes to fix seeding

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,6 @@ class User < ActiveRecord::Base
   has_many :characters
   has_many :games, through: :characters
 
-  # def initialize(name: , character_class:, race:, level:)
-  #   @name = name
-  #   @character_class = character_class
-  #   @race = race
-  #   @level = level
-  # end
+
 
 end
